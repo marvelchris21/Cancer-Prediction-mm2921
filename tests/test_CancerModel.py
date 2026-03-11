77c871f2f32a74a1d636ec5cfeb39216fbd26889
Marvel Manoharan <marvelchris1@gmail.com>
2026-03-11 14:16:47 +0000

    Added unit testing

---
+++ tests/test_CancerModel.py
@@ -0,0 +1,26 @@
+import unittest
+from cancer_predictions import cancer_model
+
+class TestCancerModel(unnitest.TestCase):
+    def test_diagnosis_to_target():
+        model = CancerModel()
+        diagnosis = 'Malignant'
+        target = model.diagnosis_to_target(diagnosis)
+        self.assertEqual(target, 0)
+
+        diagnosis = 'Benign'
+        target = model.diagnosis_to_target(diagnosis)
+        self.assertEqual(target, 1)
+
+    def test_target_to_diagnosis(self):
+        model = CancerModel()
+        target = 0
+        diagnosis = model.target_to_diagnosis(target)
+        self.assertEqual(diagnosis, 'Malignant')
+
+        target = 1
+        diagnosis = model.target_to_diagnosis(target)
+        self.assertEqual(diagnosis, 'Benign')
+
+if __name__ == '__main__':
+    unittest.main()
