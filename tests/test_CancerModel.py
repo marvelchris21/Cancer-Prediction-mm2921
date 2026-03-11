b9e06245660e9972fb4976d1d02f96e9bd82758c
Marvel Manoharan <marvelchris1@gmail.com>
2026-03-11 16:24:46 +0000

    Added pre-commit

--- tests/test_CancerModel.py
+++ tests/test_CancerModel.py
@@ -1,14 +1,16 @@
 import unittest
+
 from cancer_prediction_mm2921 import cancer_model
+
 
 class TestCancerModel(unnitest.TestCase):
     def test_diagnosis_to_target():
         model = CancerModel()
-        diagnosis = 'Malignant'
+        diagnosis = "Malignant"
         target = model.diagnosis_to_target(diagnosis)
         self.assertEqual(target, 0)
 
-        diagnosis = 'Benign'
+        diagnosis = "Benign"
         target = model.diagnosis_to_target(diagnosis)
         self.assertEqual(target, 1)
 
@@ -16,11 +18,12 @@ class TestCancerModel(unnitest.TestCase):
         model = CancerModel()
         target = 0
         diagnosis = model.target_to_diagnosis(target)
-        self.assertEqual(diagnosis, 'Malignant')
+        self.assertEqual(diagnosis, "Malignant")
 
         target = 1
         diagnosis = model.target_to_diagnosis(target)
-        self.assertEqual(diagnosis, 'Benign')
+        self.assertEqual(diagnosis, "Benign")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
